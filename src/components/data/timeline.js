a6785f1e451af032ca454f256e6eff6922b7e246
gguditus <126524383+gguditus@users.noreply.github.com>
2023-06-16 10:44:44 -0400

    spelling grammar and word choice changes

--- src/components/data/timeline.js
+++ src/components/data/timeline.js
@@ -11,14 +11,14 @@ export default [
     title: "Software Development Intern",
     duration: "1.5 years",
     details:
-      "I was invited back to work at the same company as a software development intern. I worked part-time through my second and third years of college. Also, this time ranged from the beginning through the height of the COVID-19 pandemic. During my time, I brought projects from white paper, to prototype, to production level deployment. I performed platform improvements and bug fixes on various client facing as well as internal facing parts of the codebase. Additionally, I began development on a new integration of analytics and SEO software. My favorite project was improving the performance of an internal tool that most of the company used multiple times daily. By the time I finished my changes, the load time had been reduced by 90%. Development was mainly completed in C# using the .NET framework along with JQuery, SQL, web-based API's, PowerShell, and test driven development. I also utilized git, an issue tracking software called Jira, and communication tools to collaborate with and report to co-workers.",
+      "I was invited back to work at the same company as a software development intern. I worked part-time through my second and third years of college. Also, this period ranged from the beginning through the height of the COVID-19 pandemic. During my tenure, I brought projects from white paper, to prototype, to production level deployment. I performed platform improvements and bug fixes on various client facing as well as internal facing parts of the codebase. Additionally, I began development on a new integration of analytics and SEO software. My favorite project was improving the performance of an internal tool that most of the company used multiple times daily. By the time I finished my changes, the load time had been reduced by 90%. Development was mainly completed in C# using the .NET framework along with JQuery, SQL, web-based API's, PowerShell, and test driven development. I also utilized git, an issue tracking software called Jira, and communication tools to collaborate with and report to co-workers.",
   },
   {
     year: "2019",
     title: "Software Quality Assurance Intern",
     duration: "4 months",
     details:
-      "I began working at a leading website provider for tire and automotive shops. My work during this time was mainly focused on finding and reporting bugs and issues during a client side platform update. Due to a wide variety of clients, each client site behaved quite differently and resulted in many unique bugs, as well as a number of wide reaching issues. During my time, I used a virtual machine for testing, along with Git, and the issue tracking software, Jira.",
+      "I began working at a leading website provider for tire and automotive shops. My work during this time was mainly focused on finding and reporting bugs and issues during a client side platform update. Due to a wide variety of clients, each client site behaved quite differently and resulted in many unique bugs, as well as a number of wide reaching issues. During my work, I used a virtual machine for testing, along with Git, and the issue tracking software, Jira.",
   },
   {
     year: "2018",
@@ -32,7 +32,7 @@ export default [
     title: "Hello, (Coding) World",
     duration: "2 years",
     details:
-      "I wrote my first line(s) of code in my junior year of high school in an introduction to computer science class. I really enjoyed the instant gratification of programming and building something useful out of nothing. I continued my journey the next year in an AP Computer Science class. Both of these classes focused on Java based programming. I enjoyed my time in these classes enough that I decided to pursue a degree in computer science.",
+      "I wrote my first line(s) of code in my junior year of high school in an introduction to computer science class. I really enjoyed the instant gratification of programming and building something useful out of nothing. I continued my journey the next year in an AP Computer Science class. Both of these classes focused on Java based object oriented programming. I enjoyed my time in these classes enough that I decided to pursue a degree in computer science.",
   },
   {
     year: "2000",
